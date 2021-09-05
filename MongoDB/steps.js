bd9e979508dc29ecba4e141b0cd11ab0b697e879
viviane <v.buschwallace@yahoo.com>
2021-09-05 18:59:42 +0200

    notes on Model and difference between async and promises

---
+++ MongoDB/steps.js
@@ -0,0 +1,11 @@
+// Difference between async await with try and catch and promise with then and catch
+/*
+async await with try and catch AND then and catch are both forms of promises (?)
+*/
+
+// Model
+/*
+A Model is a class that's your primary tool for interacting with MongoDB. An instance of a Model is called a Document.
+In Mongoose, the term "Model" refers to subclasses of the mongoose.Model class. You should not use the mongoose.Model class directly. The mongoose.model() and connection.model() functions create subclasses of mongoose.Model as shown below.
+source: https://mongoosejs.com/docs/api.html#Model
+*/
